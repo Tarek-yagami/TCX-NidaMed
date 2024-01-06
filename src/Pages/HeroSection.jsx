dc137462c232a4f9ed3f46c43306f8bed3a1994c
Tarek Benameur <lt_benameur@esi.dz>
2024-01-06 10:59:14 +0100

    latest changes

--- src/Pages/HeroSection.jsx
+++ src/Pages/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Health from '../Images/Health2.0.svg';
 
 export default function HeroSection() {
@@ -7,7 +8,7 @@ export default function HeroSection() {
         <p className='text-[1rem]'> Bienvenue dans l univers des médecins</p>
       <p className=' text-[3rem] font-semibold  mb-[40%]' style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.25)'}}>Simplifiez votre quotidien médical 
       <span className="text-Blue"> in NidaMed</span></p>
-      <button className='mx-[30%] py-2 px-4  bg-Blue shadow-sm  rounded-md'>Pret à debuter ?</button>
+      <Link to="/Login"><button className='mx-[30%] py-2 px-4  bg-Blue shadow-sm  rounded-md'>Pret à debuter ?</button></Link>
       </div>
       <img className='w-[50%]' src={Health} alt="imaaage" />
     </div>
